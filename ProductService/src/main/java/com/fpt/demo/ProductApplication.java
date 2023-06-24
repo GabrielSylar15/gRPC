cd2836464c7a97a124dedfb0c7c8b73134358f17
ntvinh4@cmc.com.vn <neulaanh15@gmail.com>
2023-06-25 02:35:28 +0700

    update

--- ProductService/src/main/java/com/fpt/demo/ProductApplication.java
+++ ProductService/src/main/java/com/fpt/demo/ProductApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DockerbuildApplication {
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DockerbuildApplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
